73133eb36a0318eb7ef50eb9a8854a42d0eff7dc
Dagberto Rigue <riguealive@gmail.com>
2023-07-14 11:41:34 -0300

    Add files via upload

--- 03-javascript/comentarios.js
+++ 03-javascript/comentarios.js
@@ -1,6 +1,7 @@
-// comentário de única linha
+// comentário de única linha são feitos com barra dupla
 
 /* 
 comentários maiores escritos
-em mais de uma linha
+em mais de uma linha são iniciados com barra-asterisco
+e finalizados com asterisco-barra.
 */
