74bdf739914700a37fc51e8ef4d82c26ff684b37
Dagberto Rigue <riguealive@gmail.com>
2023-07-17 10:59:10 -0300

    Add files via upload
    substituído variaveis.js

---
+++ 03-javascript/variaveis.js
@@ -0,0 +1,14 @@
+/*
+Variáveis:
+	var - declara uma variável, opcionalmente, inicializando-a com um valor.
+	let - declara uma variável local de escopo do bloco, opcionalmente, inicializando-a com um valor.
+	const - declara uma constante de escopo de bloco, apenas de leitura.
+*/
+
+/*
+Literais: são as representações binárias (que tem dois elementos -> chave + valor), formados pela declaração da variável + seu valor.
+	Por exemplo:
+*/
+	var firstName = "André";
+	let flag = true;
+	const pi = 3,14;
